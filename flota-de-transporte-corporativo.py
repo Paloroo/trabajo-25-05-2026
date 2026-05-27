a3c4586e2066a1d5024bdea97e81b341eddd6fbe
Rodrigo <rodrigoantonioroaroa@gmail.com>
2026-05-27 11:17:44 -0400

    avance

--- flota-de-transporte-corporativo.py
+++ flota-de-transporte-corporativo.py
@@ -7,5 +7,14 @@ elif cant_vehiculos >= 6:
 else:     
     print("Cantidad invalida, ingrese un numero entero positivo")
 
-placa_auto = int(input("Digito y letra de la placa: "))
-capacidad_de_carga = int(input("ingrese la cantidad de toneladas: "))
+
+for i in range(cant_vehiculos):
+    while True:
+        try:
+            placa_auto = input("Digito y letra de la placa: ")
+        except ValueError:
+            print("Invalido, Tiene un error el cual no posee letras y numeros.")
+        else: 
+             if len(placa_auto) >= 6 and " " not in placa_auto:
+                 print("Su placa es valida.")
+                 break
