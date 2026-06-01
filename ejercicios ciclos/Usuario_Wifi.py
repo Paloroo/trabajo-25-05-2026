25e4516e8f64e47a57cb5e4a536a21c9ecddf2fa
rodrigo <rodrigoantonioroaroa@gmail.com>
2026-06-01 19:45:48 -0400

    Create Usuario_Wifi.py
    ciclos job

---
+++ ejercicios ciclos/Usuario_Wifi.py
@@ -0,0 +1,25 @@
+while True:
+    try:
+        cantidad = int(input("¿Cuántos usarios desea registrar?: "))
+        if cantidad > 0:
+            break
+        print("Debe ingresar al menos 1 usuario.")
+    except ValueError:
+        print("¡Error! Debe ingresar un numero entero.")
+
+Normales = 0
+uso_alto = 0
+
+for i in range(cantidad):
+    print(f"\n--- Registro de usuario {i+1} ---")
+
+    while True:
+        nombre = input("Ingrese el nombre del usuario (minimo 4 letras y sin espacios): ")
+
+        if " " in nombre: 
+            print("El nombre no debe contener espacios.")
+        elif len(nombre):
+            print("El nombre debe contener mínimo 4 letras (caracteres).") 
+        else:
+            break
+        
